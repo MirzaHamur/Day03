daf2ba9a1a517e6ba088853496f25aa94bbc5fbf
Mirza <mirza.hamurko@gmail.com>
2021-03-10 21:44:56 +0100

    ex06

---
+++ ex06/index.js
@@ -0,0 +1,18 @@
+function main (){
+    // Only change code below this line  
+    console.log("My name is");  
+    var x;
+    var y;
+    var z;
+    x = 7;
+    y = 12;
+    z = x + "Mirza";
+
+    // Only change code below this line
+  
+    
+
+}
+main() ;
+
+module.exports = main;
