e367df0235819e62492b778a25a5935f6a35d14e
Mirza <mirza.hamurko@gmail.com>
2021-03-10 21:37:42 +0100

    ex04 fix

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,12 +1,10 @@
 function main (){
     // Only change code below this line    
-    var x = 5;
-    var y = 5;
-    var z = x + y;
+    var x = 10;
+    var y = 10;
+    return (x,y);
     // Only change code below this line
-    var p = 5;
-    var k = 5;
-    var l = p + k;
+  
     
 
 }
