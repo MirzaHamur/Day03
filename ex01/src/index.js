6e3bc433ca3d9cb904b694cd696ddbd4afceebe1
Mirza <mirza.hamurko@gmail.com>
2021-03-10 21:00:51 +0100

    EX02 Comment

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -2,6 +2,13 @@ function main (){
     // Only change code below this line    
     console.log ("Hello World");
     // Only change code below this line
+    /* document.write("This is a multi-line comment");
+    document.write("This is a multi-line comment");
+    document.write("This is a multi-line comment");
+    document.write("This is a multi-line comment");
+    document.write("This is a multi-line comment");
+    */
+   // document.write("Other type of comment")
 
 }
 main() ;
