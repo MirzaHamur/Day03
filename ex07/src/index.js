4f39429cfc3959c012b211543cdceea5a8374679
Mirza <mirza.hamurko@gmail.com>
2021-03-10 21:54:06 +0100

    ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,21 @@
+function main (){
+    // Only change code below this line  
+    var drawImage;
+    var newImageName;
+    var thisImageNameIsSoLong;
+    var StudlyCapvar;
+    var TitleCaseOver;
+
+    STUDLYCAPVAR = 10;
+    ProperCamelCase = "A string";
+    TitleCaseOver = 9000;
+    
+
+    // Only change code below this line
+  
+    
+
+}
+main() ;
+
+module.exports = main;
