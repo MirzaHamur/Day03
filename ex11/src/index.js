059437dc47535a9f3dc09cbcac481040d19f69ce
Mirza <mirza.hamurko@gmail.com>
2021-03-10 22:12:20 +0100

    fix

---
+++ ex11/src/index.js
@@ -0,0 +1,18 @@
+function main (){
+    // Only change code below this line  
+    <html>
+    <head>
+        <h1>Hello World!</h1>
+        <body>
+
+        </body>
+    </head>
+    </html>
+        
+    // Only change code below this line
+    
+
+}
+main() ;
+
+module.exports = main;
