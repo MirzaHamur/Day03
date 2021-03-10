4786521e0b963572e215b9d53585c16c2b47fed5
Mirza <mirza.hamurko@gmail.com>
2021-03-10 22:14:36 +0100

    ex12

---
+++ ex12/src/index.js
@@ -0,0 +1,19 @@
+function main (){
+    // Only change code below this line  
+    <html>
+    <head>
+        <h1>Hello World!</h1>
+        <h2>CatPhotoApp</h2>
+        <body>
+
+        </body>
+    </head>
+    </html>
+        
+    // Only change code below this line
+    
+
+}
+main() ;
+
+module.exports = main;
