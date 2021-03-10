39883e131ca415f4eb8ccd426a8214eda942fe67
Mirza <mirza.hamurko@gmail.com>
2021-03-10 21:29:30 +0100

    ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+function main (){
+    // Only change code below this line    
+    var x = 5;
+    var y = 5;
+    var z = x + y;
+    // Only change code below this line
+    var p = 5;
+    var k = 5;
+    var l = p + k;
+    
+
+}
+main() ;
+
+module.exports = main;
