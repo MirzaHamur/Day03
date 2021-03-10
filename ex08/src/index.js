a224e623b98c1230fff56a152229cd75f7778d7e
Mirza <mirza.hamurko@gmail.com>
2021-03-10 22:00:29 +0100

    ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+function main (){
+    // Only change code below this line  
+    var x = 5;
+    var y = 7;
+    var z = x + y;
+
+
+    // Only change code below this line
+  
+    
+
+}
+main() ;
+
+module.exports = main;
