1cabbeca468724194272561cd9cd8cdadaa268f9
Mirza <mirza.hamurko@gmail.com>
2021-03-10 22:28:32 +0100

    ex14

---
+++ ex15/src/index.js
@@ -0,0 +1,22 @@
+function main (){
+    // Only change code below this line  
+    <html>
+    <head>
+        <!--
+    <h1>Hello World!</h1>
+        <h2>CatPhotoApp</h2>
+        <p>Hello Paragraph</p>
+        -->
+        <body>
+
+        </body>
+    </head>
+    </html>
+    
+    // Only change code below this line
+    
+
+}
+main() ;
+
+module.exports = main;
